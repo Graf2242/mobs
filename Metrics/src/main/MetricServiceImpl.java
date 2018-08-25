b056c4850ce02333c1c4e72f5ff157dc7cb46d7d
Graf2242 <graf2242@gmail.com>
2018-08-25 21:55:19 +0300

    Add gradle Wrapper + some fixes

---
+++ Metrics/src/main/MetricServiceImpl.java
@@ -0,0 +1,174 @@
+package utils.Metrics;
+
+import base.masterService.Message;
+import base.masterService.nodes.Address;
+import base.utils.MetricsService;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.exporter.HTTPServer;
+import org.apache.logging.log4j.Logger;
+import utils.MessageSystem.NodeMessageReceiver;
+import utils.MessageSystem.NodeMessageSender;
+import utils.MessageSystem.messages.masterService.MRegister;
+import utils.ResourceSystem.ResourceFactory;
+import utils.ResourceSystem.Resources.configs.ServerConfig;
+import utils.logger.LoggerImpl;
+import utils.logger.UncaughtExceptionLog4j2Handler;
+import utils.tickSleeper.TickSleeper;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MetricServiceImpl implements MetricsService, Runnable {
+    static MetricsService metricsService = null;
+    private static Logger log;
+    private final ResourceFactory resourceFactory;
+    private final Address address = new Address();
+    private final NodeMessageReceiver messageReceiver;
+    private final Queue<Message> unhandledMessages = new LinkedBlockingQueue<>();
+    private final ServerConfig serverConfig;
+    private Map<String, Counter> counterMap = new HashMap<>();
+    private Map<String, Gauge> gaugeMap = new HashMap<>();
+    private Socket masterService;
+    private boolean masterIsReady;
+    private HTTPServer server;
+
+    public MetricServiceImpl(String configPath) {
+        this.resourceFactory = ResourceFactory.instance();
+
+        this.serverConfig = (ServerConfig) resourceFactory.getResource(configPath);
+        InetAddress address;
+        try {
+            address = InetAddress.getByName(serverConfig.getLobby().getIp());
+            masterService = new Socket(serverConfig.getMaster().getIp(),
+                    Integer.parseInt(serverConfig.getMaster().getPort()), address,
+                    Integer.parseInt(serverConfig.getLobby().getPort()));
+        } catch (IOException e) {
+            log.error(e);
+        }
+        int port=0;
+        try{
+            port = Integer.parseInt(serverConfig.getMetrics().getSecondPort());
+            if (port==0) throw new UnknownHostException();
+        } catch (UnknownHostException e) {
+            log.fatal(String.format("Unable to start metric service, %s is NAN", serverConfig.getMetrics().getSecondPort()));
+        }
+        createPrometheusService(port);
+        this.messageReceiver = new NodeMessageReceiver(unhandledMessages, masterService);
+        NodeMessageSender.sendMessage(masterService, new MRegister(this.address, MetricsService.class, serverConfig.getMetrics().getIp(), serverConfig.getMetrics().getMasterPort()));
+    }
+
+    public static void main(String[] args) {
+        String arg = null;
+        try {
+            arg = args[0];
+        } catch (Exception ignored) {
+        }
+        String configPath = Objects.equals(arg, null) ? "config.xml" : arg;
+        log = LoggerImpl.createLogger("Metrics");
+        MetricsService metricsService = new MetricServiceImpl(configPath);
+        Thread metricsThread = new Thread(metricsService);
+        metricsThread.setName("METRICS");
+        metricsThread.setUncaughtExceptionHandler(new UncaughtExceptionLog4j2Handler(log));
+        metricsThread.start();
+    }
+
+    private void createPrometheusService(int port) {
+        try {
+            server = new HTTPServer(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        TickSleeper tickSleeper = new TickSleeper();
+        //noinspection InfiniteLoopStatement
+        while (true) {
+            tickSleeper.tickStart();
+            execNodeMessages();
+            tickSleeper.tickEnd();
+        }
+    }
+
+    private void execNodeMessages() {
+
+    }
+
+    @Override
+    public void incrementCounter(String name, String help) {
+        Counter counter = getCounter(name, help);
+        counter.inc();
+    }
+
+    @Override
+    public void incrementCounter(String name, String Help, double amt) {
+        Counter counter = getCounter(name, Help);
+        counter.inc(amt);
+    }
+
+    @Override
+    public void incrementGauge(String name, String help) {
+        Gauge gauge = getGauge(name, help);
+        gauge.inc();
+    }
+
+    @Override
+    public void incrementGauge(String name, String Help, double amt) {
+        Gauge gauge = getGauge(name, Help);
+        gauge.inc(amt);
+    }
+
+    @Override
+    public void decrementGauge(String name, String help) {
+        Gauge gauge = getGauge(name, help);
+        gauge.dec();
+    }
+
+    @Override
+    public void decrementGauge(String name, String help, double amt) {
+        Gauge gauge = getGauge(name, help);
+        gauge.dec(amt);
+    }
+
+    private Counter getCounter(String name, String help) {
+        Counter counter = counterMap.get(name);
+        if (Objects.equals(counter, null)) {
+            counter = Counter.build().name(name).help(help).register();
+            counterMap.put(name, counter);
+        }
+        return counter;
+    }
+
+    private Gauge getGauge(String name, String help) {
+        Gauge gauge = gaugeMap.get(name);
+        if (Objects.equals(gauge, null)) {
+            gauge = Gauge.build().name(name).help(help).register();
+            gaugeMap.put(name, gauge);
+        }
+        return gauge;
+    }
+
+    @Override
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public void setMasterIsReady(boolean masterIsReady) {
+        this.masterIsReady=masterIsReady;
+    }
+
+    @Override
+    public Socket getMasterService() {
+        return masterService;
+    }
+}
---
+++ clientImpl/src/main/java/pages/LoginPage.java
@@ -0,0 +1,50 @@
+package pages;
+
+import utils.MessageSystem.NodeMessageSender;
+import utils.MessageSystem.messages.clientMessages.fromClient.FLogin;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.StageStyle;
+import main.ClientImpl;
+
+import java.io.IOException;
+
+public class LoginPage {
+    @FXML
+    private Button loginBtn;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField login;
+    @FXML
+    private TextField serverAddress;
+    private ClientImpl main;
+
+    public LoginPage() {
+    }
+
+    @FXML
+    private void loginBtnPressed() {
+        try {
+            String text = serverAddress.getText();
+            main.connectToServer(text);
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initStyle(StageStyle.TRANSPARENT);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText("Невозможно соединиться с сервером");
+            e.printStackTrace();
+        }
+        NodeMessageSender.sendMessage(main.getFrontendSocket(), new FLogin(login.getText(), passwordField.getText()));
+        main.waitForStatusUpdate();
+
+    }
+    public void setMain(ClientImpl main){
+        this.main = main;
+    }
+}
+
+
